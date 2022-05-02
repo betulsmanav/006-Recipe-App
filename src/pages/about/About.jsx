dbc5e02fcb13cf6d4b7d3c2dfd53b9c7965c48e1
betul <betulsmanav@gmail.com>
2022-05-02 20:30:38 +0300

    footer and about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -14,20 +14,19 @@ const About = () => {
       <StyledImage src={codingSvg} />
       <HeaderContainer>
         <h1>
-          About Software Developer <span>AshleyMiller </span>
+          About Software Developer <span>BetulSonmezManav </span>
         </h1>
       </HeaderContainer>
       <InfoContainer>
-        <h2>Hi, I'am Ashley</h2>
+        <h2>Hi, I'am Betul</h2>
         <h3>I'm currently learning Full-Stack Development Languages.</h3>
         <h4>
           
-          I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
-          Java, Hibernate, SpringBoot, AWS Services.
+          I've already known JS, ReactJS,Redux,PostgreSQL,Python,Django,Git.
         </h4>
         <h2>
-          <a href="mailto:Ashley@clarusway.com">Send email</a> :
-          Ashley@clarusway.com
+          <a href="betulsmanav@gmail.com">Send email</a> :
+          betulsmanav@gmail.com
         </h2>
       </InfoContainer>
     </AboutContainer>
@@ -35,6 +34,10 @@ const About = () => {
 };
 
 export default About;
+
+
+
+
 
 
 
